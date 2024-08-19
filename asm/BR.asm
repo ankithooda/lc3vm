7ad856a59d17c360f264edaf450fd0b026785316
Ankit <ankithooda23@gmail.com>
2024-08-19 23:57:19 +0530

    Add ASM programs for following instruction
    - BR
    - AND

--- asm/BR.asm
+++ asm/BR.asm
@@ -2,7 +2,7 @@
         ADD R0, R0, 10
         ADD R1, R1, 6
         ADD R2, R0, 11
-        BR LOCATION4
+        BRz LOCATION4
         LOCATION1 .FILL x0023
         LOCATION2 .FILL x0034
         LOCATION3 .FILL x0045
---
+++ asm/LOGICAL.asm
@@ -0,0 +1,5 @@
+        .ORIG 2000
+        ADD R0, R0, 10
+        ADD R1, R1, 13
+        AND R2, R0, R1
+        .END
