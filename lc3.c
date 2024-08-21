31e8ce2694c0c23d8d510ab82410ce56d7317ef8
Ankit <ankithooda23@gmail.com>
2024-08-21 23:12:36 +0530

    Format code according to Linux Kernel code guideline
    - Indent width 8 is the way

--- lc3.c
+++ lc3.c
@@ -8,85 +8,74 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-uint16_t
-swap16 (uint16_t value)
+uint16_t swap16(uint16_t value)
 {
-  return (value >> 8) | (value << 8);
+	return (value >> 8) | (value << 8);
 }
 
-int
-load_image_file (FILE *program)
+int load_image_file(FILE *program)
 {
-  uint16_t origin, program_sz;
-  uint16_t *load_location;
+	uint16_t origin, program_sz;
+	uint16_t *load_location;
 
-  if (fread ((void *)&origin, sizeof (uint16_t), 1, program) == -1)
-    {
-      return -1;
-    }
-  origin = swap16 (origin);
+	if (fread((void *)&origin, sizeof(uint16_t), 1, program) == -1) {
+		return -1;
+	}
+	origin = swap16(origin);
 
-  program_sz = MEMORY_MAX - origin;
-  load_location = get_memory_location (origin);
+	program_sz = MEMORY_MAX - origin;
+	load_location = get_memory_location(origin);
 
-  size_t words_read
-      = fread ((void *)load_location, sizeof (uint16_t), program_sz, program);
+	size_t words_read = fread((void *)load_location, sizeof(uint16_t),
+				  program_sz, program);
 
-  if (words_read == -1)
-    {
-      return -1;
-    }
-  else
-    {
-      // Swap each byte within each LC3 word.
-      for (uint16_t i = 0; i < words_read; i++)
-        {
-          *load_location = swap16 (*load_location);
-          load_location++;
-        }
+	if (words_read == -1) {
+		return -1;
+	} else {
+		// Swap each byte within each LC3 word.
+		for (uint16_t i = 0; i < words_read; i++) {
+			*load_location = swap16(*load_location);
+			load_location++;
+		}
 
-      set_pc (origin);
-      fprintf (stdout,
-               "Program - %ld LC3 Machine Word(s) loaded at location 0x%x\n",
-               words_read, origin);
+		set_pc(origin);
+		fprintf(stdout,
+			"Program - %ld LC3 Machine Word(s) loaded at location 0x%x\n",
+			words_read, origin);
 
-      fprintf (stdout, "Program Counter set to 0x%x\n", origin);
-      return 0;
-    }
+		fprintf(stdout, "Program Counter set to 0x%x\n", origin);
+		return 0;
+	}
 }
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+	// Initialize hardware
+	initialize_hardware();
+	fprintf(stdout, "Debug: Hardware Initialized\n");
+	debug_hardware();
 
-  // Initialize hardware
-  initialize_hardware ();
-  fprintf (stdout, "Debug: Hardware Initialized\n");
-  debug_hardware ();
+	// Load program
+	FILE *program = fopen(argv[1], "rb");
+	if (!program) {
+		fprintf(stderr, "Error: Could not open file - %s\n",
+			strerror(errno));
+		exit(2);
+	}
+	if (load_image_file(program) == -1) {
+		fprintf(stderr, "Error Could not load image in memory - %s\n",
+			strerror(errno));
+		exit(3);
+	}
 
-  // Load program
-  FILE *program = fopen (argv[1], "rb");
-  if (!program)
-    {
-      fprintf (stderr, "Error: Could not open file - %s\n", strerror (errno));
-      exit (2);
-    }
-  if (load_image_file (program) == -1)
-    {
-      fprintf (stderr, "Error Could not load image in memory - %s\n",
-               strerror (errno));
-      exit (3);
-    }
+	if (argc != 2) {
+		fprintf(stdout, "lc3-vm <image>\n");
+		exit(1);
+	}
 
-  if (argc != 2)
-    {
-      fprintf (stdout, "lc3-vm <image>\n");
-      exit (1);
-    }
+	run_machine();
 
-  run_machine ();
+	// debug_hardware();
 
-  // debug_hardware();
-
-  return 0;
+	return 0;
 }
