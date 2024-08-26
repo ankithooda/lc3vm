992f3efbc1e2cac1d8137c8bcd3ca190724d7455
Ankit <ankithooda23@gmail.com>
2024-08-26 22:28:20 +0530

    Add testing program for TRAP IN & PUTSP vectors

---
+++ asm/TRAP_IN.asm
@@ -0,0 +1,4 @@
+        .ORIG x4311
+        TRAP x23
+        TRAP x21
+        .END
---
+++ asm/TRAP_PUTSP.asm
@@ -0,0 +1,9 @@
+        .ORIG 4232
+        LEA R0, M1
+        TRAP x24
+        M1 .FILL x4142
+        M2 .FILL x4344
+        M3 .FILL x4546
+        M4 .FILL x4748
+        M5 .FILL x480a
+        .END
