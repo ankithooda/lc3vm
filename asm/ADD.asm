749da8b2bdc61cebf490dddf45bcaf8dfa95e41b
Ankit <ankithooda23@gmail.com>
2024-09-02 17:45:14 +0530

    Add testing programs for ADD and Keyboard device

--- asm/ADD.asm
+++ asm/ADD.asm
@@ -2,4 +2,7 @@
         ADD R0, R0, 11
         ADD R1, R1, 12
         ADD R2, R1, R0
+        ADD R3, R3, #-11
+        ADD R4, R4, 12
+        ADD R5, R4, R3
  .END
--- asm/KBD.asm
+++ asm/KBD.asm
@@ -9,15 +9,12 @@ LOOPA
         LEA R0, MSG1
         PUTS
         LDI R0, KBSR
-        LDI R1, KBDR
-        NOT R1, R1
-        AND R3, R1, R2
-        BRz EXIT
-        BRnzp LOOPA
+        BRn EXIT
+        BRzp LOOPA
 
         ;;  Data
-        MSG1 .STRINGZ "Press Y to repeat any other key to exit\n"
-        MSG2 .STRINGZ "Thanks for pressing Y\n"
+        MSG1 .STRINGZ "Press any other key to exit\n"
+        MSG2 .STRINGZ "Thanks for pressing a button\n"
         KBSR .FILL xFE00
         KBDR .FILL xFE02
         Y_PRESS .FILL x0059
